82d7bd214447211df14ff7e6c0e918d3d79a0ce8
JoelFaldin <joelfaldin@gmail.com>
2026-04-27 09:05:04 -0400

    feat: add round robin using go for load balancing

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -6,15 +6,42 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
+	"sync/atomic"
 )
 
+type Balancer struct {
+	upstreams []string
+	current   atomic.Uint32
+}
+
+func (b *Balancer) Next() string {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	n := b.current.Add(1)
+	wg.Done()
+
+	return b.upstreams[(int(n)-1)%len(b.upstreams)]
+}
+
 func Handler() {
-	target, err := url.Parse("http://localhost:8080")
-	if err != nil {
-		log.Fatal(err)
+	b := &Balancer{
+		upstreams: []string{"http://localhost:8080", "http://localhost:8081", "http://localhost:8082"},
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy := &httputil.ReverseProxy{}
+	proxy.Rewrite = func(pr *httputil.ProxyRequest) {
+		upstream := b.Next()
+		tr, err := url.Parse(upstream)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("server:", tr)
+
+		pr.SetURL(tr)
+		pr.SetXForwarded()
+	}
 
 	fmt.Println("Start proxy server on :3000")
 	http.ListenAndServe(":3000", proxy)
